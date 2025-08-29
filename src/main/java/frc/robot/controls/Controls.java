2c12dcced1aa0440a70e039681b8a0e4f9b07a27
balikbayan bots <hello@balikbayanbots.com>
2025-08-28 19:43:09 -0700

    Updated the controls

--- src/main/java/frc/robot/controls/Controls.java
+++ src/main/java/frc/robot/controls/Controls.java
@@ -22,18 +22,69 @@ public class Controls {
             throw new IllegalStateException("Utility class");
         }
 
-        /*** Driver 'Left X'*/
-        public static final Supplier<Double> translateX = () -> {return -driver.getLeftY() * MAX_TELEOP_SPEED;};
+        /*** Driver 'Left X' */
+        public static final Supplier<Double> translateX = () -> {
+            return -driver.getLeftY() * MAX_TELEOP_SPEED;
+        };
 
-        /*** Driver 'Left Y'*/
-        public static final Supplier<Double> translateY = () -> {return -driver.getLeftX() * MAX_TELEOP_SPEED;};
+        /*** Driver 'Left Y' */
+        public static final Supplier<Double> translateY = () -> {
+            return -driver.getLeftX() * MAX_TELEOP_SPEED;
+        };
 
-        /*** Driver 'Right X'*/
-        public static final Supplier<Double> rotate = () -> {return -driver.getRightX() * MAX_TELEOP_ROT;};
+        /*** Driver 'Right X' */
+        public static final Supplier<Double> rotate = () -> {
+            return -driver.getRightX() * MAX_TELEOP_ROT;
+        };
 
-        /*** Driver 'Start'*/
+        /*** Driver 'Start' */
         public static final Trigger reorient = driver.start();
 
-        public static final Trigger test = driver.back();
     }
+
+    public final class Debug {
+        private Debug() {
+            throw new IllegalStateException("Utility class");
+        }
+
+        public static final Trigger magnetRezero = driver.povUpRight();
+        public static final Trigger nudgeDown = coDriver.povRight();
+        public static final Trigger nudgeUp = coDriver.povLeft();
+
+    }
+
+    public class Setpoint {
+        private Setpoint() {
+            throw new IllegalStateException("Utility class");
+        }
+
+        public static final Trigger lvlOne = coDriver.x();
+
+        public static final Trigger lvlTwo = coDriver.a();
+
+        public static final Trigger lvlThree = coDriver.b();
+
+        public static final Trigger lvlFour = coDriver.y();
+
+        public static final Trigger stow = coDriver.povDown();
+
+    }
+
+    public class Climb {
+        private Climb() {
+            throw new IllegalStateException("Utility class");
+        }
+
+        public static final Trigger deploy = coDriver.back();
+    }
+
+    public class Manipulators {
+        private Manipulators() {
+            throw new IllegalStateException("Utility class");
+        }
+
+        public static final Trigger intake = coDriver.leftBumper();
+        public static final Trigger score = driver.rightTrigger();
+    }
+
 }
