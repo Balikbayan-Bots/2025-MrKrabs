fbf085f4d675629cce7dd37629572b5d686c18cf
Michael <gozzook@gmail.com>
2025-09-12 18:44:59 -0700

    set level 2 setpoint back to 60.0

--- src/main/java/frc/robot/subsystems/body/BodySetpoint.java
+++ src/main/java/frc/robot/subsystems/body/BodySetpoint.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.body;
 
 public enum BodySetpoint {
     CORAL_LEVEL1(0.0, 90.0),
-    CORAL_LEVEL2(8.5,170.0),
+    CORAL_LEVEL2(8.5,60.0),
     CORAL_LEVEL3(17.0, 60.0),
     CORAL_LEVEL4(25.0,60.0),
     ALGAE_LEVEL2(12.0,90.0),
