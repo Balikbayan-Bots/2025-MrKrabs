d04abcda3f17d91a60a60ad47244f31b9e0dbf91
PenguinGang <penguingang915@gmail.com>
2025-09-14 16:11:46 -0700

    Temp (#29)
    * tuned PIDS to lower error amounts
    
    * fix heinous imports
    
    ---------
    
    Co-authored-by: Eryk <bluelightbmx@gmail.com>

--- src/main/java/frc/robot/subsystems/body/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/body/ArmSubsystem.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.CoastOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
